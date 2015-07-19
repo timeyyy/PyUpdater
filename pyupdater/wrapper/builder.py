458cdc6c47d478d479fa2ee1995bc8444897ceb5
JMSwag <johnymoswag@gmail.com>
2015-07-19 15:13:34 -0700

    RTM

--- pyupdater/wrapper/builder.py
+++ pyupdater/wrapper/builder.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # --------------------------------------------------------------------------
+from __future__ import unicode_literals
+
 import logging
 import optparse
 import os
@@ -25,7 +27,6 @@ from pyupdater.hooks import get_hook_dir
 from pyupdater.utils import (check_repo,
                              lazy_import,
                              make_archive,
-                             run,
                              Version)
 
 from PyInstaller.main import run as pyi_build
@@ -97,11 +98,11 @@ class Builder(object):
         # Check for spec file or python script
         app_info = self._check_input_file(self.pyi_args)
         self._setup()
-        spec_file_path = os.path.join(self.spec_dir, temp_name + u'.spec')
+        spec_file_path = os.path.join(self.spec_dir, temp_name + '.spec')
 
         # Spec file used instead of python script
-        if app_info[u'type'] == u'spec':
-            spec_file_path = app_info[u'name']
+        if app_info['type'] == 'spec':
+            spec_file_path = app_info['name']
         else:
             # Creating spec file from script
             self._make_spec(self.args, self.pyi_args, temp_name, app_info)
@@ -111,7 +112,7 @@ class Builder(object):
         # Archive executable
         self._archive(self.args, temp_name)
         finished = time.time() - start
-        log.info(u'Build finished in {:.2f} seconds.'.format(finished))
+        log.info('Build finished in {:.2f} seconds.'.format(finished))
 
     def make_spec(self):
         temp_name = jms_utils.system.get_system()
@@ -122,32 +123,32 @@ class Builder(object):
     def _setup(self):
         # Create required directories
         self.pyi_dir = os.path.join(os.getcwd(), settings.USER_DATA_FOLDER)
-        self.new_dir = os.path.join(self.pyi_dir, u'new')
+        self.new_dir = os.path.join(self.pyi_dir, 'new')
         self.build_dir = os.path.join(os.getcwd(), settings.CONFIG_DATA_FOLDER)
-        self.spec_dir = os.path.join(self.build_dir, u'spec')
-        self.work_dir = os.path.join(self.build_dir, u'work')
+        self.spec_dir = os.path.join(self.build_dir, 'spec')
+        self.work_dir = os.path.join(self.build_dir, 'work')
         for d in [self.build_dir, self.spec_dir, self.work_dir,
                   self.pyi_dir, self.new_dir]:
             if not os.path.exists(d):
-                log.debug(u'Creating directory: {}'.format(d))
+                log.debug('Creating directory: {}'.format(d))
                 os.mkdir(d)
 
     # Ensure that a spec file or python script is present
     def _check_input_file(self, pyi_args):
         verified = False
         for p in pyi_args:
-            if p.endswith(u'.py'):
-                log.debug(u'Building from python source file: {}'.format(p))
-                app_info = {u'type': u'script', u'name': p}
+            if p.endswith('.py'):
+                log.debug('Building from python source file: {}'.format(p))
+                app_info = {'type': 'script', 'name': p}
                 verified = True
                 break
-            elif p.endswith(u'.spec'):
-                log.debug(u'Building from spec file: {}'.format(p))
-                app_info = {u'type': u'spec', u'name': p}
+            elif p.endswith('.spec'):
+                log.debug('Building from spec file: {}'.format(p))
+                app_info = {'type': 'spec', 'name': p}
                 verified = True
                 break
         if verified is False:
-            log.error(u'Must pass a python script or spec file')
+            log.error('Must pass a python script or spec file')
             sys.exit(1)
         return app_info
 
@@ -159,28 +160,28 @@ class Builder(object):
 
         if args.console is True or args.nowindowed is True \
                 or args._console is True:
-            if u'-c' not in spec_args:
+            if '-c' not in spec_args:
                 log.debug('Adding -c to pyi args')
-                spec_args.append(u'-c')
+                spec_args.append('-c')
         if args.windowed is True or args.noconsole is True \
                 or args._windowed is True:
-            if u'-w' not in spec_args:
+            if '-w' not in spec_args:
                 log.debug('Adding -w to pyi args')
-                spec_args.append(u'-w')
+                spec_args.append('-w')
         # Ensure that onefile mode is passed
-        spec_args.append(u'-F')
-        spec_args.append(u'--name={}'.format(temp_name))
+        spec_args.append('-F')
+        spec_args.append('--name={}'.format(temp_name))
         if spec_only is True:
             log.debug('User generated spec file')
-            spec_args.append(u'--specpath={}'.format(os.getcwd()))
+            spec_args.append('--specpath={}'.format(os.getcwd()))
         else:
             # Place spec file in .pyupdater/spec
-            spec_args.append(u'--specpath={}'.format(self.spec_dir))
+            spec_args.append('--specpath={}'.format(self.spec_dir))
         # Use hooks included in PyUpdater package
         hook_dir = get_hook_dir()
         log.debug('Hook directory: {}'.format(hook_dir))
-        spec_args.append(u'--additional-hooks-dir={}'.format(hook_dir))
-        spec_args.append(app_info[u'name'])
+        spec_args.append('--additional-hooks-dir={}'.format(hook_dir))
+        spec_args.append(app_info['name'])
 
         log.debug('Make spec cmd: {}'.format(' '.join([c for c in spec_args])))
         pyi_makespec(spec_args)
@@ -201,10 +202,10 @@ class Builder(object):
             sys.exit(1)
         build_args = []
         if args.clean is True:
-            build_args.append(u'--clean')
-        build_args.append(u'--distpath={}'.format(self.new_dir))
-        build_args.append(u'--workpath={}'.format(self.work_dir))
-        build_args.append(u'-y')
+            build_args.append('--clean')
+        build_args.append('--distpath={}'.format(self.new_dir))
+        build_args.append('--workpath={}'.format(self.work_dir))
+        build_args.append('-y')
         build_args.append(spec_file_path)
 
         log.debug('Build cmd: {}'.format(''.join([b for b in build_args])))
@@ -214,24 +215,24 @@ class Builder(object):
 
     # Updates name of binary from mac to applications name
     def _mac_binary_rename(self, temp_name, app_name):
-        bin_dir = os.path.join(temp_name, u'Contents', u'MacOS')
-        plist = os.path.join(temp_name, u'Contents', u'Info.plist')
+        bin_dir = os.path.join(temp_name, 'Contents', 'MacOS')
+        plist = os.path.join(temp_name, 'Contents', 'Info.plist')
         with jms_utils.paths.ChDir(bin_dir):
             # ToDo: Shouldn't be hard coded. Better solution needed
-            os.rename(u'mac', app_name)
+            os.rename('mac', app_name)
 
         # We also have to update to ensure app launches correctly
-        with open(plist, u'r') as f:
+        with open(plist, 'r') as f:
             plist_data = f.readlines()
 
         new_plist_data = []
         for d in plist_data:
-            if u'mac' in d:
-                new_plist_data.append(d.replace(u'mac', app_name))
+            if 'mac' in d:
+                new_plist_data.append(d.replace('mac', app_name))
             else:
                 new_plist_data.append(d)
 
-        with open(plist, u'w') as f:
+        with open(plist, 'w') as f:
             for d in new_plist_data:
                 f.write(d)
 
@@ -239,26 +240,26 @@ class Builder(object):
     def _archive(self, args, temp_name):
         # Now archive the file
         with jms_utils.paths.ChDir(self.new_dir):
-            if os.path.exists(temp_name + u'.app'):
-                log.debug(u'Got mac .app')
-                app_name = temp_name + u'.app'
+            if os.path.exists(temp_name + '.app'):
+                log.debug('Got mac .app')
+                app_name = temp_name + '.app'
                 name = args.app_name
                 self._mac_binary_rename(app_name, name)
-            elif os.path.exists(temp_name + u'.exe'):
-                log.debug(u'Got win .exe')
-                app_name = temp_name + u'.exe'
+            elif os.path.exists(temp_name + '.exe'):
+                log.debug('Got win .exe')
+                app_name = temp_name + '.exe'
                 name = args.app_name
             else:
                 app_name = temp_name
                 name = args.app_name
             version = args.app_version
             log.debug('Temp Name: {}'.format(temp_name))
-            log.debug(u'Appname: {}'.format(app_name))
+            log.debug('Appname: {}'.format(app_name))
             log.debug('Version: {}'.format(version))
 
             # Time for some archive creation!
             file_name = make_archive(name, version, app_name)
-            log.debug(u'Archive name: {}'.format(file_name))
+            log.debug('Archive name: {}'.format(file_name))
             if args.keep is False:
                 if os.path.exists(temp_name):
                     log.debug('Removing: {}'.format(temp_name))
@@ -272,4 +273,4 @@ class Builder(object):
                         os.remove(app_name)
                     else:
                         shutil.rmtree(app_name, ignore_errors=True)
-        log.info(u'{} has been placed in your new folder\n'.format(file_name))
+        log.info('{} has been placed in your new folder\n'.format(file_name))
