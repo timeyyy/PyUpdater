e5d2133589339e56a6302d809ec448baf675cd98
JMSwag <johnymoswag@gmail.com>
2015-07-26 17:57:14 -0700

    correctly add file path to logger

--- pyupdater/wrapper/__init__.py
+++ pyupdater/wrapper/__init__.py
@@ -70,8 +70,9 @@ log.addHandler(sh)
 db = Storage()
 loader = Loader(db)
 LOG_DIR = user_log_dir(settings.APP_NAME, settings.APP_AUTHOR)
-rfh = logging.handlers.RotatingFileHandler(LOG_DIR, 'pyu.log',
-                                           maxBytes=30000, backupCount=3)
+log_file = os.path.join(LOG_DIR, 'pyu.log')
+rfh = logging.handlers.RotatingFileHandler(log_file, maxBytes=30000,
+                                           backupCount=3)
 rfh.setFormatter(log_formatter())
 rfh.setLevel(logging.DEBUG)
 log.addHandler(rfh)
